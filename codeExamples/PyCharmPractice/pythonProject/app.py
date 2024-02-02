605cd8d87a0094468a2d974c818f987cc5d3dbb6
Mistie <143767309+MystKitteh@users.noreply.github.com>
2024-02-02 16:57:04 -0500

    New folder
    New folder for my semester projects! Also finally got Spacy to work

---
+++ codeExamples/PyCharmPractice/pythonProject/app.py
@@ -0,0 +1,9 @@
+course = "Python for Beginnners"
+print(course.upper())
+print(course)
+print(course.replace('for', "4"))
+# 01234 not 1234
+print("Python" in course)
+
+price = 5
+print(price > 10 or price < 30)
